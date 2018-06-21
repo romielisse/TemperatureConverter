cbbf425871d3bb3fff6ec25fdcbe88251ba95981
Melissa Fung <romielisse@gmail.com>
2018-06-21 10:53:44 -0400

    adding images

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
 
-var cel, fah;
+var cel, fah, img_src;
 
 // Convert to Celsius
 function toCelsius(fahrenheit) {
      cel.value = (5/9) * (fahrenheit-32);
 	 fah.value = "";
+	 
+	 if(cel.value < 0){
+		 document.getElementById("picture").src = "img/snowman.png";
+	 }
 }
 
 // Convert to Fahrenheit
 function toFahrenheit(celcius) {
 	fah.value = celcius * (9/5) + 32;
 	cel.value = "";
+	
+	if(fah.value < 32){
+		document.getElementById("picture").src = "img/snowman.png";
+	}
 }
   
 function temp() {
